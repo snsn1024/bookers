938e25bacef45de2f08113290b7f98955b2d7862
snsn1024 <47966782+snsn1024@users.noreply.github.com>
2019-03-13 20:36:31 +0900

    index+new

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,4 @@
 class PostsController < ApplicationController
-  def new
-  	@post = Post.new
-  end
 
   def create
 	post = Post.new(post_params)
@@ -15,6 +12,7 @@ class PostsController < ApplicationController
 
   def index
   	@posts = Post.all
+  	@post = Post.new
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do	
-  get 'top' => 'root#top'
   root :to => "root#top"
   resources :posts
   delete 'posts/:id' => 'posts#destroy', as: 'destroy_post'
