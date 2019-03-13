3cb88ca2b51466db79ce4987ebfb83ecbe02025e
snsn1024 <47966782+snsn1024@users.noreply.github.com>
2019-03-13 16:16:15 +0900

    fit editform

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,7 +6,7 @@ class PostsController < ApplicationController
   def create
 	post = Post.new(post_params)
 	post.save
-	redirect_to '/top'
+	redirect_to '/post'
   end
 
   def index
@@ -18,13 +18,17 @@ class PostsController < ApplicationController
   end
 
   def edit
-  	@post = Post.find(params[id])
+  	@post = Post.find(params[:id])
   end
 
   def update
   	post = Post.find(params[:id])
   	post.update(post_params)
   	redirect_to post_path(post.id)
+  end
+
+  def destroy
+
   end
 
 private
--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/new'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'top' => 'root#top'
-
-post 'posts'=> 'posts#create'
-
-get 'posts' => 'posts#index'
-
-get 'post/:id' => 'posts#show', as: 'post'
-
-get 'posts:id/edit' => 'posts#edit', as: 'edit_post'
-
-patch 'posts/:id' => 'posts#update', as: 'update_post'
+  get 'top' => 'root#top'
+  resources :posts
 end
+
